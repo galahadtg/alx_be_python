b394b6196c9c9142c1814d0ab6bdb5ee0244d652
James Arthur Appiah <james.galahad@gmail.com>
2025-10-05 22:16:26 +0000

    Add BankAccount class and CLI script main-0.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -31,5 +31,5 @@ class BankAccount:
         return True
 
     def display_balance(self):
-        """Display the current account balance."""
-        print(f"Current Balance: ${self.__account_balance}")
+        """Display the current account balance with two decimal places."""
+        print(f"Current Balance: ${self.__account_balance:.2f}")
